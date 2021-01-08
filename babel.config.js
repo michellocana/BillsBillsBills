1202be98139a662e5e61981811f238d7bcbb7550
Michell Ocaña <ocana.1997@hotmail.com>
2021-01-07 23:59:39 -0300

    feat: making .env file configurable

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [['module:react-native-dotenv']]
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: process.env.DOT_ENV_FILE ? `.env.${process.env.DOT_ENV_FILE}` : '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true
+      }
+    ]
+  ]
 }
