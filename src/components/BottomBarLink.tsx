7a088aa7b023b148ab26b291a8e326f27292a495
Michell Ocaña <ocana.1997@hotmail.com>
2021-01-17 03:05:16 -0300

    feat: animating BottomBarLink color on route change

--- src/components/BottomBarLink.tsx
+++ src/components/BottomBarLink.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native'
+import React, { useEffect, useRef } from 'react'
+import { Animated, StyleSheet, TouchableNativeFeedback, View } from 'react-native'
 import { useHistory, useLocation } from 'react-router-native'
 import Icon from 'react-native-vector-icons/Feather'
 
 import { COLOR_GREEN_1, COLOR_WHITE } from '../constants/colors'
+import { DEFAULT_ANIMATION_DURATION } from '../constants/animation'
 
 const s = StyleSheet.create({
   link: {
@@ -14,17 +15,12 @@ const s = StyleSheet.create({
 
   text: {
     fontSize: 12,
-    color: COLOR_WHITE,
-    marginTop: 4
-  },
-
-  textActive: {
-    color: COLOR_GREEN_1
+    flexDirection: 'column'
   },
 
   icon: {
     fontSize: 24,
-    marginTop: 0
+    marginBottom: 4
   }
 })
 
@@ -34,17 +30,35 @@ type BottomBarLinkProps = {
   children: string
 }
 
-// TODO animate color transition
 export default function BottomBarLink({ to, icon, children }: BottomBarLinkProps) {
   const history = useHistory()
   const location = useLocation()
   const isActive = location.pathname === to
+  const color = useRef(new Animated.Value(Number(isActive))).current
+  const AnimatedIcon = useRef(Animated.createAnimatedComponent(Icon)).current
+  const animatedColor = {
+    color: color.interpolate({
+      inputRange: [0, 1],
+      outputRange: [COLOR_WHITE, COLOR_GREEN_1]
+    })
+  }
+
+  useEffect(() => {
+    Animated.timing(color, {
+      toValue: Number(isActive),
+      duration: DEFAULT_ANIMATION_DURATION,
+      useNativeDriver: false
+    }).start()
+  }, [isActive])
 
   return (
     <TouchableNativeFeedback onPress={() => history.push(to)}>
       <View style={s.link}>
-        <Icon name={icon} size={30} style={[s.text, s.icon, isActive && s.textActive]} />
-        <Text style={[s.text, isActive && s.textActive]}>{children}</Text>
+        <Animated.Text style={animatedColor}>
+          <AnimatedIcon name={icon} size={30} style={[s.icon]} />
+        </Animated.Text>
+
+        <Animated.Text style={[s.text, animatedColor]}>{children}</Animated.Text>
       </View>
     </TouchableNativeFeedback>
   )
